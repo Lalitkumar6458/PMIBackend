001e5f6f84c812c5a1d72626fef17060189b81ec
Lalit kumar <kumar932687@gmail.com>
2023-09-10 14:30:23 +0530

    user token send

--- Routes/GoogleLogin.js
+++ Routes/GoogleLogin.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../Database/Modals/User");
 const ConnectMongo = require("../Database/conn");
-
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 ConnectMongo();
 router.post("/", async (req, res) => {
@@ -16,7 +17,7 @@ router.post("/", async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
 
     const resData = {
       username: user.name,
@@ -26,7 +27,7 @@ router.post("/", async (req, res) => {
     // Generate a JSON Web Token (JWT) for authentication
    
 
-    res.status(200).json({ message: "Login successful", data: resData });
+    res.status(200).json({ message: "Login successful",token, data: resData });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
